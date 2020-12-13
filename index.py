2760895b5c9fe640ac48131f6b4eb464728a9b40
Paul Okello <paulokello273@gmail.com>
2020-12-13 09:38:42 +0300

    Gathering training and testing data

--- index.py
+++ index.py
@@ -14,16 +14,32 @@ from sklearn.model_selection import RandomizedSearchCV, cross_val_score
 
 #Reading the data
 df = pd.read_csv("winequality-white.csv", delimiter=";")
-df.head()
-print(df.head())
+
 
 #Visual Analysis of the 🍷 
 
-for label in df.columns[:-1]:
-   #for label in  ["alcohol"]:
-    plt.boxplot([df[df['quality'] == i][label] for i in range(1,11)]) 
-    plt.title(label)
-    plt.xlabel("Quality")
-    plt.ylabel(label)
-    plt.savefig("imgs/"+"white".join(label.split(" ")))
-    plt.show()
+# for label in df.columns[:-1]:
+#    #for label in  ["alcohol"]:
+#     plt.boxplot([df[df['quality'] == i][label] for i in range(1,11)]) 
+#     plt.title(label)
+#     plt.xlabel("Quality")
+#     plt.ylabel(label)
+#     plt.savefig("imgs/"+"white".join(label.split(" ")))
+#     plt.show()
+    
+#Gathering training and testing data 
+bins = [0,5.5,7.5,10]
+labels = [0,1,2]
+df["quality"] = pd.cut(df["quality"], bins=bins, labels=labels)
+df.head()
+print(df.head())
+
+x = df[df.columns[:-1]]
+y = df["quality"]
+sc = StandardScaler()
+x = sc.fit_transform(x)
+
+x_train, x_test, y_train, y_test = train_test_split(x,y ,test_size=.2, random_state=42)
+
+for data in (y_train, y_test):
+    print(data.describe())
